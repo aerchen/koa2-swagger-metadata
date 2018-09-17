5f7e4f7fc244fb7ecef6ecaf8eb1b55a4f717bb8
chenjiangang <15012937056@163.com>
2018-09-17 10:26:54 +0800

    增加validator

--- index.js
+++ index.js
@@ -5,7 +5,9 @@
 
 'use strict';
 
+const _ = require('lodash');
 const swaggerTools = require('swagger-tools');
+const validate = require('jsonschema').validate;
 const debug = require('debug')('koa2-swagger-metadata');
 
 exports = module.exports = async function () {
@@ -26,12 +28,47 @@ exports = module.exports = async function () {
     });
   };
 
-  return async function swaggerMetadata (ctx, next) {
+  const swaggerMetadata = async function (ctx, next) {
+    debug('Parse metadata');
     await parseMetadata(ctx);
 
     if (ctx.req.swagger) {
+      debug('Parsed params', ctx.req.swagger.params);
       ctx.swagger = ctx.req.swagger;
+      delete ctx.req.swagger;
     }
+
     await next();
+  };
+
+  const metadataValidator = async function (ctx, next) {
+    debug('Validate metadata');
+    const {swagger} = ctx;
+
+    // skip validate since params not exist
+    if (!(swagger && swagger.params)) {
+      return await next();
+    }
+
+    // validate params
+    _.forEach(swagger.params, (param, paramName) => {
+      const {value, schema} = param;
+      const valid = validate(value, schema.schema || schema);
+      if (!valid.valid) {
+        debug('Validate failed');
+        debug(paramName, param, valid);
+
+        const err = valid.errors[0];
+        err.property = err.property.replace('instance', paramName);
+        throw err;
+      }
+    });
+    debug('Validate success');
+    await next();
+  };
+
+  return {
+    swaggerMetadata,
+    metadataValidator
   };
 };
